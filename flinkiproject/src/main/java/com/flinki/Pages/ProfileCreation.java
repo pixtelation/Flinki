680a38bf85a17d2c860f64f30b9fe1bee13aa164
sahabaj <sahabaj.ahammed2022@gmail.com>
2025-03-27 18:35:14 +0530

    git changes new pages

--- flinkiproject/src/main/java/com/flinki/Pages/ProfileCreation.java
+++ flinkiproject/src/main/java/com/flinki/Pages/ProfileCreation.java
@@ -3,24 +3,27 @@ package com.flinki.Pages;
 import java.util.List;
 import java.util.Random;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
-import com.github.javafaker.Faker;
 
 import com.flinki.Base.base;
+import com.flinki.Resources.Generic;
+import com.github.javafaker.Faker;
 
 
 public class ProfileCreation extends base {
-     SignUp su = new SignUp(driver);    
-     Faker faker = new Faker();
+    SignUp su = new SignUp(driver);
+    Faker faker = new Faker();
+    Generic Profile = new Generic();
+    
 
-
-    public ProfileCreation(WebDriver driver)
-    {
-        this.driver=driver;
+    public ProfileCreation(WebDriver driver) {
+        this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
@@ -34,58 +37,108 @@ public class ProfileCreation extends base {
     WebElement Country;
     @FindBy(name = "nationality")
     WebElement Nationality;
+
+    @FindBy(xpath = "//ul[@class='check-group-wrap']/li[not(label[normalize-space()='Other'])]")
+    List<WebElement> sportsIntersted;
+
+    @FindBy(xpath = "//button[normalize-space()='Save & Next']")
+    private WebElement SaveNextButton;
+
+    @FindBy(xpath = "//input[@name='dateOfBirth']")
+    private WebElement datOfBirth;
+
+    @FindBy(xpath = "//button[normalize-space()='Save & Next']")
+    private WebElement saveNextButton;
+
+    @FindBy(xpath = "//button[normalize-space()='Next']")
+    private WebElement nextButton;
+
+    @FindBy(xpath = "//button[normalize-space()='Add New Race/Event']")
+    private WebElement addNewRaceEvent;
+
+    @FindBy(xpath = "//div[@class='css-19bb58m']")
+    private WebElement typeOfCreate;
+
+    @FindBy(xpath = "//input[@placeholder='Start date']")
+    private WebElement startDate;
+    @FindBy(xpath = "//input[@placeholder='End date']")
+    private WebElement endDate;
     
 
-     /////////////////////////////////////////////////////////////////////Methods////////////////////////////////////////////////////////////////////////////////////////
-    public void PersonalInformation() throws Exception
-    {
+
+    /////////////////////////////////////////////////////////////////////Methods////////////////////////////////////////////////////////////////////////////////////////
+    public ProfileCreation PersonalInformation() throws Exception {
         // su.SignupWithTCfx();///////Signup function is processsed
         FirstName.sendKeys(faker.name().firstName());
         LastName.sendKeys(faker.name().lastName());
+        Thread.sleep(3000);
         ////////Country dropdown
         Select selectcountry = new Select(Country);
         List<WebElement> optionsCountry = selectcountry.getOptions();
-        if (optionsCountry.size() > 0 && optionsCountry.get(0).getText().equals("Select Country")) {
-            optionsCountry.remove(0);
+        Thread.sleep(3000);
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(optionsCountry.size() - 1) + 1; // Avoid index 0 if it's a placeholder
+        selectcountry.selectByIndex(randomIndex);
+        datOfBirth.sendKeys("27/02/2010");
+        saveNextButton.click();
+        return this;
+
+    }
+
+    /*---------------------------------------------------------RACE  & EVENTS------------------------------------------------------------------------- */
+    public ProfileCreation RacesEvents()
+
+    {
+        Random rand = new Random();
+
+        for (int i = 0; i < 4; i++) {
+            int randomIndex = rand.nextInt(sportsIntersted.size()); // Pick a random index
+            WebElement sport = sportsIntersted.get(randomIndex);
+            sport.click(); // Click the random sport
+            sportsIntersted.remove(randomIndex); // Remove to avoid duplicate clicks
         }
 
-        if (optionsCountry.size() > 0) {
-            Random random = new Random();
-            int randomIndex = random.nextInt(optionsCountry.size());
-            WebElement randomOption = optionsCountry.get(randomIndex);
-            selectcountry.selectByVisibleText(randomOption.getText());
-            System.out.println("SelectedCountry: " + randomOption.getText()); //Optional, for verification
-        } else {
-            System.out.println("Dropdown has no options.");
-        }
+        SaveNextButton.click();
 
+        return this;
+    }
 
-        ////////Nationality dropdown
-        Select select = new Select(Nationality);
-        List<WebElement> options = select.getOptions();
-        if (options.size() > 0 && options.get(0).getText().equals("Select Country")) {
-            options.remove(0);
-        }
+    /*-----------------------------ADD NEW RACE EVENT [If user click  this button ]--------------------------- */
 
-        if (options.size() > 0) {
-            Random random = new Random();
-            int randomIndex = random.nextInt(options.size());
-            WebElement randomOption = options.get(randomIndex);
-            select.selectByVisibleText(randomOption.getText());
-            System.out.println("SelectedNationality: " + randomOption.getText()); //Optional, for verification
-        } else {
-            System.out.println("Dropdown has no options.");
-        }
+    public void addNewraceEvent()
 
+    {   Profile.clickElement(addNewRaceEvent);
+        Profile.enterText(typeOfCreate ,Profile.randomstring());
+        Profile.enterText(startDate, "20/05/2025");
+        Profile.enterText(endDate, "01/06/2025");
+        Profile.scrollToBottom();
 
 
 
         
 
 
-
     }
-    
-   
-    
+
+    /* ----------------------------------------SportsQualifications-----------------------------------------------*/
+    public ProfileCreation SportsQualifications() throws InterruptedException {
+        nextButton.click();
+        nextButton.click();
+        nextButton.click();
+        nextButton.click();
+        // Locate the "Standard Plan" section using XPath
+        WebElement standardPlanSection = driver.findElement(
+                By.xpath("//div[contains(@class, 'jsx-facee91221e6c5e6') and contains(@class, 'col-md-6')]"));
+
+        // Scroll the section into view
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView({block: 'center'});", standardPlanSection);
+
+        // Wait to ensure the section is fully visible
+        Thread.sleep(2000);
+        
+
+        return this;
+    }
+
 }
